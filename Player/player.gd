57cfb89906246997169d97af7e5970768b3973f5
Rodrigo (Spy) Domingues <rodrigod@hiperlogic.com.br>
2022-06-10 14:07:24 -0300

    player workings

--- Player/player.gd
+++ Player/player.gd
@@ -1,11 +1,50 @@
 extends RigidBody2D
 
+enum States {INIT, ALIVE, INVULNERABLE, DEAD}
+var state = null
+
+export (int) var engine_power
+export (int) var spin_power
+
+var thrust = Vector2()
+var rotation_dir = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	change_state(States.ALIVE)
 
+func change_state(new_state):
+	### TODO: Should check if the current to new state transition is ok
+	### Set up the new state properties
+	match new_state:
+		States.INIT:
+			$CollisionShape2D.disabled = true
+		States.ALIVE:
+			$CollisionShape2D.disabled = false
+		States.INVULNERABLE:
+			$CollisionShape2D.disabled = true
+		States.DEAD:
+			$CollisionShape2D.disabled = true
+	state = new_state
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+	get_input()
 #	pass
+
+func get_input():
+	thrust = Vector2()
+	if state in [States.DEAD, States.INIT]:
+		return
+	if Input.is_action_pressed("thrust"):
+		thrust = Vector2(engine_power, 0)
+	rotation_dir=0
+	if Input.is_action_pressed("rotate_right"):
+		rotation_dir+=.1
+	if Input.is_action_pressed("rotate_left"):
+		rotation_dir -=.1
+		
+func _physics_process(delta):
+	set_applied_force(thrust.rotated(rotation))
+	set_applied_torque(spin_power * rotation_dir)
+	pass
